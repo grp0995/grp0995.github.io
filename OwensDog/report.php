30e79e942e95aed6cbf1c2485b18b4647e246a95
grp0995 <grp0995@gmail.com>
2023-08-14 11:42:00 -0500

    OwnDog

---
+++ OwensDog/report.php
@@ -0,0 +1,8 @@
+<?php
+$when-happend = $POST['firstName'];
+$how-long = $POST['lastName'];
+
+echo 'Thanks for submittint the form. <br />';
+echo 'You were abducted ' . $whenhappend;
+echo 'and were gone for ' .$how-long;
+?>
